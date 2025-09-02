ddcc999380b3fa97e5138d866ad4f7eea93e64e4
sohan2503001 <sohanjambhule973@gmail.com>
2025-09-02 20:59:51 +0530

    week-3 started -> added firebase intialization

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,3 +2,19 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  // add more env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
